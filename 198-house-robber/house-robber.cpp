6c22a9269b2d8c495590d1182411702b24da0246
Lohitaksha Negi <lohitakshx@gmail.com>
2025-04-10 11:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int loki(int i , vector<int>& nums, vector<int>& dp){
-        if(i>=nums.size()) return 1;
-        if (dp[i] != -1) return dp[i];
-
-        
-        dp[i] = max(nums[i]+loki(i+2, nums, dp), loki(i+1, nums, dp));
-        return dp[i];
-    };
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return loki(0,nums,dp)-1;
-        
+        if(nums.size()==1) return nums[0];
+        vector<int> dp(nums.size());
+        dp[0]= nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i<= nums.size()-1; i++){
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1]);
+            
+        }
+        return dp[nums.size()-1];
     }
 };
