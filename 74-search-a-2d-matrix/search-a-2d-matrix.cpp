b31a8f321b828d9f5baa9e15aa9f685a8b7519dc
Lohitaksha Negi <lohitakshx@gmail.com>
2025-04-10 13:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (43.41%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (auto& row : matrix) {
-            if (target >= row.front() && target <= row.back()) {
-                if (binary_search(row.begin(), row.end(), target)) return true;
-            }
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int left = 0;
+        int right = n*m-1;
+
+        while(left<=right){
+            int mid = (left+right)/2;
+            int val = matrix[mid/m][mid%m];
+            if(val==target) return true;
+            else if(val<target) left=mid+1;
+            else right = mid-1;
         }
         return false;
     }
