5279324e7b0f2418bfb28f3255d235dcd924ccf7
Lohitaksha Negi <lohitakshx@gmail.com>
2025-04-10 12:29:28 +0530

    Time: 11 ms (98.07%) | Memory: 18 MB (57.98%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0) return 0;
-        queue<int> q;
-        unordered_set<int> visited;
+        vector<int> dp(amount + 1, amount + 1); // using amount+1 as "infinity"
+        dp[0] = 0;
 
-        q.push(0);
-        visited.insert(0);
-        int steps = 0;
-
-        while (!q.empty()) {
-            int size = q.size();
-            ++steps;
-
-            for (int i = 0; i < size; ++i) {
-                int curr = q.front(); q.pop();
-
-                for (int coin : coins) {
-                    int next = curr + coin;
-                    if (next == amount) return steps;
-                    if (next < amount && visited.find(next) == visited.end()) {
-                        q.push(next);
-                        visited.insert(next);
-                    }
-                }
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
             }
         }
 
-        return -1; // not possible
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
