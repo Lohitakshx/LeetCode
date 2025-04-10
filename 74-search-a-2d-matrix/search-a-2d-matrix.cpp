3d838bebfc303ad729e174bc34e5363547140d7a
Lohitaksha Negi <lohitakshx@gmail.com>
2025-04-10 12:41:37 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.75%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto& row : matrix) {
+            for (int val : row) {
+                if (val == target) return true;
+            }
+        }
+        return false;
+    }
+};
