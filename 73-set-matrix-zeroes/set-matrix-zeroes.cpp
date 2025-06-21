ea8de463b4d2a9bfebfcf592366c6d7f3cf19bd6
Lohitaksha Negi <lohitakshx@gmail.com>
2025-06-21 18:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (50.98%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -7,7 +7,6 @@ public:
         // vector<int> row(n, 0);
         // vector<int> col(m, 0);
         int col0=1;
-
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(matrix[i][j] == 0) {
