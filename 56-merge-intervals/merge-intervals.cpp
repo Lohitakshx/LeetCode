613e196054f8c404db7c9c496a002b12eb05478f
Lohitaksha Negi <lohitakshx@gmail.com>
2025-08-08 15:06:15 +0530

    Time: 4 ms (69.56%) | Memory: 23.9 MB (57.29%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,18 +4,8 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         for(int i = 0; i<intervals.size(); i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) {
-                continue;
-            }
-            for(int j = i+1; j<intervals.size(); j++){
-                if(intervals[j][0]<=end){
-                    end = max(intervals[j][1], end);
-                }
-                else break;
-            }
-            ans.push_back({start, end});
+           if(ans.empty() || intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
+           else ans.back()[1] = max(ans.back()[1], intervals[i][1]);
         }
         return ans;
     }
