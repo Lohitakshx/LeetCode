75eab615862ed35282d3f5d5281414eda9a71ac8
Lohitaksha Negi <lohitakshx@gmail.com>
2025-08-29 20:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.33%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long sum = n+m-1;
+        long long top = n+m-2;
+        long long bottom = n-1;
         long long ans = 1;
-        for(int i = 1; i<m; i++){
-            ans = ans * (sum-m+i)/i;
+        for(int i = 1; i<=bottom; i++){
+            ans = ans * (top-bottom+i)/i;
         }    
         return ans;
     }
