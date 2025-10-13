cf74d7eb7be730c2ae197bed2744b4100eebe787
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-13 16:49:58 +0530

    Time: 30 ms (90.12%) | Memory: 53.5 MB (75.53%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    bool isPalindrome(string &arr){
-        string copy = arr;
-        reverse(arr.begin(), arr.end());
-        return arr == copy;
-    }
-    void loki (string s, int idx, vector<vector<string>>& ans, vector<string>& q){
-        if(idx == s.length()){
-            ans.push_back(q);
-            return;
-        } 
-        for(int i = idx; i<s.length(); i++){
-            string temp = s.substr(idx, i-idx+1);
-            if(isPalindrome(temp)){
-                 q.push_back(temp);
-                 loki(s, i+1, ans, q);
-                 q.pop_back();
+    vector<vector<string>> partition(string s) {
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // Initialize the DP table for single characters and pairs
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+        for (int length = 2; length <= n; ++length) {
+            for (int i = 0; i <= n - length; ++i) {
+                int j = i + length - 1;
+                if (s[i] == s[j] && (length == 2 || dp[i + 1][j - 1])) {
+                    dp[i][j] = true;
+                }
             }
         }
+
+        vector<vector<string>> result;
+        vector<string> path;
+        backtrack(s, 0, path, result, dp);
+        return result;
     }
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> q;
-        loki(s, 0, ans, q);
-        return ans;
+
+private:
+    void backtrack(const string& s, int start, vector<string>& path, vector<vector<string>>& result, const vector<vector<bool>>& dp) {
+        // If we've reached the end of the string, add the current partition to the result list
+        if (start == s.length()) {
+            result.push_back(path);
+            return;
+        }
+        // Explore all possible partitions
+        for (int end = start; end < s.length(); ++end) {
+            // Use the DP table to check if the substring s[start:end+1] is a palindrome
+            if (dp[start][end]) {
+                path.push_back(s.substr(start, end - start + 1));
+                // Recur to find other partitions
+                backtrack(s, end + 1, path, result, dp);
+                // Backtrack to explore other partitions
+                path.pop_back();
+            }
+        }
     }
 };
