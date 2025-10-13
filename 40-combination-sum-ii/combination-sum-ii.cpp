ea580fb680f315b2336e446fb66d0f0edcb6925c
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-14 01:39:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (82.45%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,34 +1,25 @@
+
 class Solution {
 public:
-    void solve(int idx, int n, vector<int>& candidates, int target,
-               int sum, vector<int>& q, vector<vector<int>>& ans) {
-
-        // base case
-        if (sum == target) {
+    void loki(vector<int>& candidates, int target, int sum, int n, vector<vector<int>>& ans, vector<int>& q, int idx){
+        if(sum==target){
             ans.push_back(q);
             return;
         }
-        if (idx >= n || sum > target) return;  // stop exploring
-
-        // ✅ CHOOSE current element
-        q.push_back(candidates[idx]);
-        solve(idx + 1, n, candidates, target, sum + candidates[idx], q, ans);
-        q.pop_back();
-
-        // ✅ NOT CHOOSE current element
-        // skip all duplicates of the current number before next recursion
-        int nextIdx = idx + 1;
-        while (nextIdx < n && candidates[nextIdx] == candidates[idx]) {
-            nextIdx++;
+        for(int i = idx ; i<n; i++){
+            if(i>idx && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target-sum) break;
+            q.push_back(candidates[i]);
+            loki(candidates, target, sum+candidates[i],n, ans, q, i+1);
+            q.pop_back();
         }
-        solve(nextIdx, n, candidates, target, sum, q, ans);
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
+        int n = candidates.size();
         vector<vector<int>> ans;
         vector<int> q;
-        solve(0, candidates.size(), candidates, target, 0, q, ans);
+        sort(candidates.begin(), candidates.end());
+        loki(candidates, target, 0, n, ans, q, 0);
         return ans;
     }
 };
