ed9f59add675a2c648beb5829494cd780489e146
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-14 14:57:36 +0530

    Time: 47 ms (93.00%) | Memory: 8.8 MB (65.20%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,31 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board){
-        for(int row = 0; row< board.size(); row++){
-            for(int col = 0; col<board[0].size(); col++){
-                if(board[row][col]=='.'){
-                    for(char c = '1'; c<='9'; c++){
-                        if(isValid(row, col, board, c)){
-                            board[row][col]=c;
-                            if(solve(board)) return true;
-                            else board[row][col] = '.';
-                        }
-                    }
-                    return false;
+    // Boolean arrays to keep track of constraints
+    bool row[9][10];   // row[i][num] = true if num exists in row i
+    bool col[9][10];   // col[j][num] = true if num exists in col j
+    bool box[9][10];   // box[k][num] = true if num exists in box k
+
+    void solveSudoku(vector<vector<char>>& board) {
+        // Initialize constraints arrays
+        memset(row, false, sizeof(row));
+        memset(col, false, sizeof(col));
+        memset(box, false, sizeof(box));
+
+        // Fill constraints based on initial board
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int num = board[i][j] - '0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    box[(i/3)*3 + j/3][num] = true;
                 }
             }
         }
-        return true;
+
+        // Start solving
+        solve(board, 0, 0);
     }
-    bool isValid(int row, int col, vector<vector<char>>& board, char c){
-        for(int i = 0; i<9; i++){
-            if(board[row][i]==c) return false;
-            if(board[i][col]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+
+private:
+    bool solve(vector<vector<char>>& board, int i, int j) {
+        if(j == 9){ // Move to next row
+            i++; j = 0;
         }
-        return true;
-    }
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        if(i == 9) return true; // Solved entire board
+
+        if(board[i][j] != '.') return solve(board, i, j+1); // Skip filled cells
+
+        for(int num = 1; num <= 9; num++){
+            if(!row[i][num] && !col[j][num] && !box[(i/3)*3 + j/3][num]){
+                // Place number
+                board[i][j] = num + '0';
+                row[i][num] = col[j][num] = box[(i/3)*3 + j/3][num] = true;
+
+                if(solve(board, i, j+1)) return true; // Recurse
+
+                // Backtrack
+                board[i][j] = '.';
+                row[i][num] = col[j][num] = box[(i/3)*3 + j/3][num] = false;
+            }
+        }
+
+        return false; // No valid number found, backtrack
     }
 };
