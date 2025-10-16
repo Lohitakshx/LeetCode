9a8d1da18a00433df7d2f3a5937fc31bf2e1a257
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-16 13:57:56 +0530

    Time: 8 ms (14.60%) | Memory: 23.7 MB (10.44%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,16 +1,36 @@
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> tokens;
-        string token;
-        stringstream ss(s);
-        while(ss>>token){
-            tokens.push_back(token);
+        int left = 0, right = s.length() - 1;
+
+        // Step 1: Trim leading and trailing spaces
+        while (left <= right && s[left] == ' ') left++;
+        while (right >= left && s[right] == ' ') right--;
+
+        string word = "";
+        string ans = "";
+
+        // Step 2: Traverse and build words
+        while (left <= right) {
+            char ch = s[left];
+            if (ch != ' ') {
+                word += ch;
+            } else if (word != "") {  // encountered space after a word
+                // prepend word to answer
+                if (!ans.empty()) ans = word + " " + ans;
+                else ans = word;
+                word = "";
+            }
+            left++;
         }
-        string ans;
-        for(int i = tokens.size()-1; i>=0; i--){
-            ans+=tokens[i];
-            if(i>0) ans+= ' ';
+
+        // Step 3: Add the last word
+        if (!word.empty()) {
+            if (!ans.empty()) ans = word + " " + ans;
+            else ans = word;
         }
 
         return ans;
