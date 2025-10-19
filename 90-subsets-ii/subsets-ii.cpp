268328d9561385a360c1a5cedb1248dbe9f8685e
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-19 14:08:04 +0530

    Time: 6 ms (10.44%) | Memory: 14.4 MB (14.01%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,24 @@
 class Solution {
   public:
   
-    void loki(vector<int> &arr, int idx, vector<int>& curr, vector<vector<int>> &ans ){
-        ans.push_back(curr);
-        for(int i =idx; i<arr.size(); i++){
-            if(i!=idx && arr[i]==arr[i-1]) continue;
-            curr.push_back(arr[i]);
-            loki(arr, i+1, curr, ans);
-            curr.pop_back();
+    void loki(vector<int> &arr, int idx, vector<int> &curr, set<vector<int>> &ans ){
+        if(idx == arr.size()) {
+            ans.insert(curr);
+            return;
         }
+        loki(arr, idx+1, curr, ans);
+        curr.push_back(arr[idx]);
+        loki(arr, idx+1, curr,ans);
+        curr.pop_back();
         
     }
     vector<vector<int>> subsetsWithDup(vector<int>& arr) {
         vector<int>curr;
         sort(arr.begin(), arr.end());
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         loki(arr,0, curr, ans);
-        return ans;
+        vector<vector<int>> ans1;
+        for(auto i : ans) ans1.push_back(i);
+        return ans1;
     }
 };
