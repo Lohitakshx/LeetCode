3abcfeb7f676eaf6df521ab6a95862fb6086fd67
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-24 15:21:58 +0530

    Time: 151 ms (39.95%) | Memory: 14.4 MB (48.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int n, int idx, int pre,vector<vector<int>> &dp){
-        if(idx==n) return 0;
-        if(dp[idx][pre+1]!=-1) return dp[idx][pre+1];
-        int notTake = solve(nums, n, idx+1, pre, dp);
-        int take = 0;
-        if(pre==-1 || nums[idx]>nums[pre]){
-            take = 1+solve(nums, n, idx+1, idx, dp);
-        }
-        return dp[idx][pre+1] = max(take, notTake);
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int idx = 0;
-        int pre = -1;
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return solve(nums, n, idx, pre ,dp);
+        vector<int> next(n+1, 0), curr(n+1, 0);
+        for(int idx = n-1; idx>=0; idx--){
+            for(int pre = idx-1; pre>=-1; pre--){
+                int notTake = next[pre+1];
+                int take = 0;
+                if(pre==-1 || nums[idx]>nums[pre]){
+                    take = 1+next[idx+1];
+                }
+                curr[pre+1] = max(take, notTake);
+            }
+            next = curr;
+        }
+        return next[0];
     }
 };
