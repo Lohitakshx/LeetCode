fc4e607bfc8c211b8cca6b3fef7584b045a6169d
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-25 13:24:43 +0530

    Time: 2063 ms (5.00%) | Memory: 471.8 MB (5.62%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,22 +6,17 @@ public:
         int n = nums.size();
     
         for(int i = 0; i < n-2; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue; // skip duplicates
-            int l = i+1, r = n-1;
-            while(l < r) {
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum == 0) {
-                    res.push_back({nums[i], nums[l], nums[r]});
-                    l++; r--;
-                    // skip duplicates
-                    while(l < r && nums[l] == nums[l-1]) l++;
-                    while(l < r && nums[r] == nums[r+1]) r--;
-                    }   
-                else if(sum < 0) l++;
-                else r--;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            unordered_set<int> s;
+            for(int j = i+1; j < n; j++) {
+                int complement = -nums[i] - nums[j];
+                if(s.count(complement)) {
+                    res.push_back({nums[i], nums[j], complement});
+                    while(j+1 < n && nums[j] == nums[j+1]) j++; // skip duplicates
+                }
+                s.insert(nums[j]);
             }
         }
-        return res;
+    return res;
     }
 };
-
