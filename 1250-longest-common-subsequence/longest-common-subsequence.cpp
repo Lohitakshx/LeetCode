e19292c4be22378e68918121e44a95a650c41470
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-27 23:45:38 +0530

    Time: 19 ms (85.93%) | Memory: 27.2 MB (77.72%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string a, string b) {
-    int n = a.size(), m = b.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            if(a[i-1] == b[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
-    }
-    return dp[n][m];
-}
+    // int loki(int n , int m, string text1, string text2){
+    //     if(n==-1||m==-1) return 0;
+    //     if(text1[n]==text2[m]) return 1 + loki(n-1, m-1, text1, text2);
+    //     return max(loki(n-1, m, text1, text2), loki(n, m-1, text1, text2));
 
+    // }
+    // int loki1(int )
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(text1[i]==text2[j]) dp[i+1][j+1]=1+dp[i][j];
+                else dp[i+1][j+1]=max(dp[i+1][j], dp[i][j+1]);
+            }
+
+        }
+        // for(auto i: dp){
+
+        // }
+        return dp[n][m];
+    }
 };
