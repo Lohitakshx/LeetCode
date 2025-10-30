a76f793a0cf8c1d6bb7c5301562acaf62d273a88
Lohitaksha Negi <lohitakshx@gmail.com>
2025-10-31 02:11:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (11.23%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        int n = s.size();
+        int m = t.size();
+        if(n!=m) return false;
+        unordered_map<char , int> arr;
+        for(int i = 0 ; i< n; i++){
+            arr[s[i]]++;
+        }
+        for(int i = 0; i<n; i++){
+            arr[t[i]]--;
+            if(arr[t[i]]<0) return false;
+        }
+        return true;
     }
 };
