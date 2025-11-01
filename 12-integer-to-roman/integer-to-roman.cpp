a3afd4143f1aa665539a224a5f4eb9fad7788633
Lohitaksha Negi <lohitakshx@gmail.com>
2025-11-02 02:17:21 +0530

    Time: 29 ms (5.13%) | Memory: 23.9 MB (5.17%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        unordered_map<int, string> roman = {
+            {3000, "MMM"},
+            {2000, "MM"},
+            {1000, "M"},
+            {900, "CM"},
+            {800, "DCCC"},
+            {700, "DCC"},
+            {600, "DC"},
+            {500, "D"},
+            {400, "CD"},
+            {300, "CCC"},
+            {200, "CC"},
+            {100, "C"},
+            {90, "XC"},
+            {80, "LXXX"},
+            {70, "LXX"},
+            {60, "LX"},
+            {50, "L"},
+            {40, "XL"},
+            {30, "XXX"},
+            {20, "XX"},
+            {10, "X"},
+            {9, "IX"},
+            {8, "VIII"},
+            {7, "VII"},
+            {6, "VI"},
+            {5, "V"},
+            {4, "IV"},
+            {3, "III"},
+            {2, "II"},
+            {1, "I"},
+        };
+        string ans = "";
+        int z= 1;
+        while(num!=0){
+            ans=roman[(num%10)*z] + ans;
+            z*=10;
+            num/=10;
+        }
+        return ans;
+    }
+};
