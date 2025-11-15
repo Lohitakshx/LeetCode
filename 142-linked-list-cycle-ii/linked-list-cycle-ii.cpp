415ecf74724253bc470a87246eb706f22e8bd2f4
Lohitaksha Negi <lohitakshx@gmail.com>
2025-11-15 13:01:33 +0530

    Time: 7 ms (66.44%) | Memory: 13.4 MB (10.19%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,28 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-      
-        ListNode* slow  = head;
-        ListNode* fast = head;
-        while(fast && fast->next){
-            fast= fast->next->next;
-            slow = slow->next;
-            if(fast==slow) break; //funfact : this collsion point distance from the head is the length of the cycle
+        unordered_set<ListNode*> seen;
+
+        while (head) {
+            if (seen.find(head) != seen.end()) {
+                return head;
+            }
+            seen.insert(head);
+            head = head->next;
         }
-        if (!fast || !fast->next) return nullptr;
-        fast = head; // ans now when you ake fast to head: they will meet at collion only
-        while (fast != slow) {
-            fast = fast->next;
-            slow = slow->next;
-        }
-        return slow;
+
+        return nullptr;
     }
 };
