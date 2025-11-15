0154c67d4db591d443a257feaa9f25d2a4ffbe52
Lohitaksha Negi <lohitakshx@gmail.com>
2025-11-15 13:01:15 +0530

    Time: 3 ms (98.01%) | Memory: 11.3 MB (82.87%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -15,10 +15,10 @@ public:
         while(fast && fast->next){
             fast= fast->next->next;
             slow = slow->next;
-            if(fast==slow) break;
+            if(fast==slow) break; //funfact : this collsion point distance from the head is the length of the cycle
         }
         if (!fast || !fast->next) return nullptr;
-        fast = head;
+        fast = head; // ans now when you ake fast to head: they will meet at collion only
         while (fast != slow) {
             fast = fast->next;
             slow = slow->next;
