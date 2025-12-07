81961c637ed3aa34a9a8e222194ecbede8b3f6cb
Lohitaksha Negi <lohitakshx@gmail.com>
2025-12-07 13:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (99.76%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -3,8 +3,8 @@ public:
     int minAddToMakeValid(string s) {
         int open = 0;
         int close = 0;
-        for(auto i: s){
-            if(i=='(') open++;
+        for(auto i : s){
+            if(i == '(') open++;
             else {
                 if(open>0) open--;
                 else close++;
