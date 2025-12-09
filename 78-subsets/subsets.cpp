98c5da94b7b956ce8317875cfe35e9dd85293235
Lohitaksha Negi <lohitakshx@gmail.com>
2025-12-09 17:17:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.67%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& ans,
-               vector<int>& temp, int idx) {
-
-        ans.push_back(temp);  // ✅ every state is a valid subset
-
-        for (int i = idx; i < nums.size(); i++) {
-            temp.push_back(nums[i]);        // choose
-            solve(nums, ans, temp, i + 1);  // explore
-            temp.pop_back();                // backtrack
+    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& temp, int idx){
+        if(idx==nums.size()) {
+            ans.push_back(temp);
+            return;
         }
+        temp.push_back(nums[idx]); // take
+        solve(nums, ans, temp, idx+1); // take
+        temp.pop_back();
+        solve(nums, ans, temp, idx+1); // not take 
+        
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
