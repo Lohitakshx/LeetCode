5f89b44e11562bd0548c631c491894af1c24c432
Lohitaksha Negi <lohitakshx@gmail.com>
2025-12-09 13:08:09 +0530

    Time: 15 ms (65.60%) | Memory: 58 MB (67.19%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -11,31 +11,29 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* mid = head;
-        ListNode* end = head;
-        while(end->next && end->next->next){
-            mid = mid->next;
-            end = end->next->next;
-        }
-        ListNode* midNext = mid->next;
-        mid ->next = NULL;
-        ListNode* left = sortList(head);
-        ListNode* right = sortList(midNext);
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        while(left && right){
-            if(left -> val <= right -> val){
-                temp -> next = left;
-                left = left->next;
-            }
-            else{
-                temp -> next = right;
-                right = right -> next;
-            }
+                vector<int> arr;
+
+        // Pointer to traverse the list
+        ListNode* temp = head;
+
+        // Traverse and push values into vector
+        while (temp != nullptr) {
+            arr.push_back(temp->val);
             temp = temp->next;
         }
-        temp->next = left ? left : right;
-        return dummy->next;
+
+        // Sort the vector
+        sort(arr.begin(), arr.end());
+
+        // Reassign sorted values to list nodes
+        temp = head;
+        for (int i = 0; i < arr.size(); i++) {
+            temp->val = arr[i];
+            temp = temp->next;
+        }
+
+        // Return head of sorted list
+        return head;
+
     }
 };
