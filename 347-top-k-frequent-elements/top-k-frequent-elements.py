cb4a6eb5f5297c65e8978a44027d315227d91ae7
Lohitaksha Negi <lohitakshx@gmail.com>
2026-02-12 11:37:31 +0530

    Time: 3 ms (90.37%) | Memory: 23.1 MB (30.88%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,11 +3,11 @@ class Solution:
         count = {}
         for i in nums:
             count[i] = 1 + count.get(i,0)
-        arr = []
+        minHeap = []
         for num, freq in count.items():
-            arr.append([freq,num])
-        arr.sort()
+            heapq.heappush(minHeap, [freq,num])
+            if len(minHeap)>k: heapq.heappop(minHeap)
         res = []
-        while len(res)<k:
-            res.append(arr.pop()[1])
+        while minHeap:
+            res.append(heapq.heappop(minHeap)[1])
         return res
