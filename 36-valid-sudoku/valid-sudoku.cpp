c91c6c2bc43aa558f56168aa65f0261f9a87dda7
Lohitaksha Negi <lohitakshx@gmail.com>
2026-02-20 17:14:25 +0530

    Time: 9 ms (16.44%) | Memory: 23 MB (53.02%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // 3 arrays to track numbers in rows, columns, and boxes
-        bool row[9][9] = {false};
-        bool col[9][9] = {false};
-        bool box[9][9] = {false};
+        vector<vector<bool>> row(9, vector<bool>(9, false));
+        vector<vector<bool>> col(9, vector<bool>(9, false));
+        vector<vector<bool>> box(9, vector<bool>(9, false));
 
-        for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++) {
-                if(board[i][j] == '.') continue;
-
-                int num = board[i][j] - '1'; // convert char '1'-'9' to index 0-8
-                int k = (i / 3) * 3 + (j / 3); // box index 0-8
-
-                if(row[i][num] || col[j][num] || box[k][num])
-                    return false; // duplicate found
-
+        for(int i = 0; i<9;i++){
+            for(int j = 0; j<9; j++){
+                if(board[i][j]=='.') continue;
+                int num = board[i][j]-'1';
+                int k = (i/3) * 3 + (j/3);
+                if(row[i][num] || col[j][num] || box[k][num]) return false;
                 row[i][num] = col[j][num] = box[k][num] = true;
             }
         }
