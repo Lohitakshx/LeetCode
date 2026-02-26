f9fd8ae3014b26fc3e403afcd01ef730ca6d2aab
Lohitaksha Negi <lohitakshx@gmail.com>
2026-02-26 09:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.22%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,6 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = [[]]
-        for num in nums:
-            ans += [subset + [num] for subset in ans]
+        ans = []
+        n = len(nums)
+        for mask in range(1<<n):
+            subset = []
+            for i in range(n):
+                if mask & (1<<i):
+                    subset.append(nums[i])
+            ans.append(subset.copy())
         return ans
