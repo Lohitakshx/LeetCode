93e98c69a6cf411ac7d8f3e6713163692af52617
Lohitaksha Negi <lohitakshx@gmail.com>
2026-03-24 17:02:12 +0530

    Time: 7 ms (96.10%) | Memory: 19.8 MB (56.22%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,39 +1,22 @@
 class Solution:
-    def isSafe(self, board, row, col, n) -> bool:
-        temp_row = row
-        temp_col = col
-        while row >= 0 and col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            col-=1
-            row-=1
-        row, col = temp_row, temp_col
-        while col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            col-=1
-        row, col = temp_row, temp_col
-        while row < n and col >= 0:
-            if board[row][col] == 'Q':
-                return False
-            col-=1
-            row+=1
-        return True
-
+    def solve(self, ans, board, n, col, upperD, leftCol, lowerD):
+        if col == n : 
+            ans.append(["".join(row) for row in board])
+            return 
+        for row in range(n):
+            if row not in leftCol and row+col not in upperD and row-col not in lowerD:
+                leftCol.add(row)
+                upperD.add(row+col)
+                lowerD.add(row-col)
+                board[row][col] = 'Q'
+                self.solve(ans, board, n, col+1, upperD, leftCol, lowerD)
+                leftCol.remove(row)
+                upperD.remove(row+col)
+                lowerD.remove(row-col)
+                board[row][col] = '.'
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans = []
         board = [['.'] * n for _ in range(n)]
-
-        def solve(col):
-            if col == n : 
-                ans.append(["".join(row) for row in board])
-                return
-            for row in range(n):
-                if self.isSafe(board, row, col, n):
-                    board[row][col] = 'Q'
-                    solve(col+1)
-                    board[row][col] = '.'
-
-        solve(0)
+        self.solve(ans, board, n, 0, set(), set(), set())
         return ans
