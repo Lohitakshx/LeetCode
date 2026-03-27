da36c7384a878b7ce70d3d53cb5b78392c008ec2
Lohitaksha Negi <lohitakshx@gmail.com>
2026-03-27 16:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (83.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -4,10 +4,9 @@ class Solution:
         return max(self.helper(nums[1:]), self.helper(nums[:-1]))
 
     def helper(self, nums: List[int]) -> int:
-        dp = [0] * len(nums)
-        if len(nums) == 0 : return 0
-        if len(nums) == 1 : return nums[0]
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        return dp[len(nums)-1] 
+        prev1 , prev2 = 0, 0
+        for num in nums:
+            curr = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
