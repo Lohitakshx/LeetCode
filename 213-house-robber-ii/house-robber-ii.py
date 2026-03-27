fbf967d550ae91dabf01f48d991b3546dc09da8a
Lohitaksha Negi <lohitakshx@gmail.com>
2026-03-27 16:18:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (83.05%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,15 +1,25 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1 : return nums[0]
-        def helper(start, end) -> int:
-            prev1 , prev2 = 0, 0
-            for i in range(start, end+1):
-                curr = max(prev1, prev2 + nums[i])
-                prev2 = prev1
-                prev1 = curr
-            return prev1
+    def rob(self, nums):
+        n = len(nums)
+        
+        if n == 1:
+            return nums[0]
+        
+        def helper(start, end):
+            dp = [0] * n
+            dp[start] = nums[start]
+            
+            for i in range(start + 1, end + 1):
+                take = nums[i]
+                if i - 2 >= start:
+                    take += dp[i - 2]
+                
+                skip = dp[i - 1]
+                dp[i] = max(take, skip)
+            
+            return dp[end]
+        
         return max(
-            helper(0, len(nums)-2), helper(1, len(nums) - 1)
+            helper(0, n - 2),  # exclude last
+            helper(1, n - 1)   # exclude first
         )
-
-    
